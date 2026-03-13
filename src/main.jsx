17a0ea5dbeb4523c5a2ebafed43034dc2625ab45
Tousif Khan <tousifkhanweb@gmail.com>
2026-03-14 01:22:58 +0530

    fontawesome and bootstrap install

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import "./assets/mediaquary.css";
 import App from "./App.jsx";
