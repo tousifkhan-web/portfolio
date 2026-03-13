05121c6843bed5c8ba8ff9527185138db3c884fe
Tousif Khan <tousifkhanweb@gmail.com>
2026-03-14 01:29:57 +0530

    google font is installed using npm

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/lato";
+import "@fontsource/poppins";
+import "@fontsource-variable/inter";
+import "@fontsource-variable/caveat";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
